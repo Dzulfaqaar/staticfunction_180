c8163a37bb3c4b9e32daa45c0af7aebceb9f9477
dzulfaqaar <fikri6aul@gmail.com>
2023-05-15 19:25:04 +0700

    definisi member function

---
+++ destruktor_180/destruktor_180.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class angka {
+private:
+	int* arr;
+	int panjang;
+public:
+	angka(int); //constructor
+	~angka(); //destructor
+	void cetakData();
+	void isiData();
+};
+//definisi member function
+angka::angka(int i) { //constructor
+	panjang = i;
+	arr = new int[i];
+	isiData();
+}
+
+angka::~angka() { //destructor
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "Alamat Array sudah dilepaskan" << endl;
+}
+
