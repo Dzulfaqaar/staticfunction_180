82cd4e0b9ba9c2c42275816fd1c4a40950e1615a
dzulfaqaar <fikri6aul@gmail.com>
2023-05-15 15:34:26 +0700

    void mahasiswa

--- staticfunction_180/staticfunction_180.cpp
+++ staticfunction_180/staticfunction_180.cpp
@@ -13,3 +13,12 @@ public:
 	mahasiswa(string pnama) :nama(pnama) { setID(); }
 };
 
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID() {
+	id = ++nim;
+}
+
+void mahasiswa::printAll() {
+	cout << "ID =" << id << endl;
+}
