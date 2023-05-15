d0b2d30b6a86adac897496727cc0feb7e5b165cb
dzulfaqaar <fikri6aul@gmail.com>
2023-05-15 19:37:31 +0700

    mahasiswa

--- konstruktor_180/konstruktor_180.cpp
+++ konstruktor_180/konstruktor_180.cpp
@@ -13,3 +13,21 @@ public:
 	mahasiswa(int iNIm, string iNama);
 	void cetak();
 };
+
+mahasiswa::mahasiswa() {
+	nim = 0;
+	nama = "";
+}
+
+mahasiswa::mahasiswa(int iNim) {
+	nim = iNim;
+}
+
+mahasiswa::mahasiswa(string iNama) {
+	nama = iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama) {
+	nim = iNim;
+	nama = iNama;
+}
